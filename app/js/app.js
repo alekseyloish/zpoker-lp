d82279c1bea13141b284e5bdd2fef161c679400a
oleksii.loish <oloish@rockher.com>
2019-12-03 19:02:36 +0200

    added subscribe form handler

--- app/js/app.js
+++ app/js/app.js
@@ -10763,28 +10763,92 @@
 	//select2($);
 
 	$(document).ready(function () {
+	  (function ($) {
+	    $.fn.serializeObject = function () {
+	      var self = this,
+	          json = {},
+	          push_counters = {},
+	          patterns = {
+	        "validate": /^[a-zA-Z][a-zA-Z0-9_]*(?:\[(?:\d*|[a-zA-Z0-9_]+)\])*$/,
+	        "key": /[a-zA-Z0-9_]+|(?=\[\])/g,
+	        "push": /^$/,
+	        "fixed": /^\d+$/,
+	        "named": /^[a-zA-Z0-9_]+$/
+	      };
+
+	      this.build = function (base, key, value) {
+	        base[key] = value;
+	        return base;
+	      };
+
+	      this.push_counter = function (key) {
+	        if (push_counters[key] === undefined) {
+	          push_counters[key] = 0;
+	        }
+
+	        return push_counters[key]++;
+	      };
+
+	      $.each($(this).serializeArray(), function () {
+	        // Skip invalid keys
+	        if (!patterns.validate.test(this.name)) {
+	          return;
+	        }
+
+	        var k,
+	            keys = this.name.match(patterns.key),
+	            merge = this.value,
+	            reverse_key = this.name;
+
+	        while ((k = keys.pop()) !== undefined) {
+	          // Adjust reverse_key
+	          reverse_key = reverse_key.replace(new RegExp("\\[" + k + "\\]$"), ''); // Push
+
+	          if (k.match(patterns.push)) {
+	            merge = self.build([], self.push_counter(reverse_key), merge);
+	          } // Fixed
+	          else if (k.match(patterns.fixed)) {
+	              merge = self.build([], k, merge);
+	            } // Named
+	            else if (k.match(patterns.named)) {
+	                merge = self.build({}, k, merge);
+	              }
+	        }
+
+	        json = $.extend(true, json, merge);
+	      });
+	      return json;
+	    };
+	  })(jQuery);
+
 	  $('#subscribe_submit').on('click', function (e) {
-	    /*e.preventDefault();
-	    		var email = $('#subscriber_email').val();
-	    var formData = $('.subscribe-form').serializeArray();
-	    		console.log( formData );
-	    		if ( email !== '' ) {
-	    			$.ajax({
-	    		type: 'POST',
-	    		url: 'http://api.zpoker.uk/public/pre_sign_up',
-	    		data: formData,
-	    		crossDomain: true,
-	    		dataType: 'jsonp',
-	    		success: function(data) {
-	    			console.log(data);
-	    		},
-	    		failure: function(errMsg) {
-	    			console.log(errMsg);
-	    		}
-	    	});
-	    		} else {
-	    	console.log('Email field is empty!');
-	    }*/
+	    e.preventDefault();
+	    let email = $('#subscriber_email').val(); //var formData = $('.subscribe-form').serializeArray();
+
+	    let formData = $('.subscribe-form').serializeObject();
+	    console.log(formData);
+
+	    if (email !== '') {
+	      $.ajax({
+	        type: 'POST',
+	        url: 'http://api.zpoker.uk/public/pre_sign_up',
+	        data: formData,
+	        // crossDomain: true,
+	        dataType: 'json',
+	        success: function (data) {
+	          console.log(data);
+	        },
+	        failure: function (errMsg) {
+	          console.log(errMsg);
+	        }
+	      });
+	    } else {
+	      console.log('Email field is empty!');
+	      $('.subscribe-form__input').addClass('error');
+	      setTimeout(function () {
+	        $('.subscribe-form__input').removeClass('error');
+	      }, 1000);
+	    }
 	  });
 	});
 	$(window).on('load', function () {
--- src/js/app.js
+++ src/js/app.js
@@ -13,11 +13,79 @@ import AOS from 'aos';
 
 $(document).ready(function () {
 
-	$('#subscribe_submit').on('click', function (e) {
-		/*e.preventDefault();
+    (function($){
+        $.fn.serializeObject = function(){
 
-		var email = $('#subscriber_email').val();
-		var formData = $('.subscribe-form').serializeArray();
+            var self = this,
+                json = {},
+                push_counters = {},
+                patterns = {
+                    "validate": /^[a-zA-Z][a-zA-Z0-9_]*(?:\[(?:\d*|[a-zA-Z0-9_]+)\])*$/,
+                    "key":      /[a-zA-Z0-9_]+|(?=\[\])/g,
+                    "push":     /^$/,
+                    "fixed":    /^\d+$/,
+                    "named":    /^[a-zA-Z0-9_]+$/
+                };
+
+
+            this.build = function(base, key, value){
+                base[key] = value;
+                return base;
+            };
+
+            this.push_counter = function(key){
+                if(push_counters[key] === undefined){
+                    push_counters[key] = 0;
+                }
+                return push_counters[key]++;
+            };
+
+            $.each($(this).serializeArray(), function(){
+
+                // Skip invalid keys
+                if(!patterns.validate.test(this.name)){
+                    return;
+                }
+
+                var k,
+                    keys = this.name.match(patterns.key),
+                    merge = this.value,
+                    reverse_key = this.name;
+
+                while((k = keys.pop()) !== undefined){
+
+                    // Adjust reverse_key
+                    reverse_key = reverse_key.replace(new RegExp("\\[" + k + "\\]$"), '');
+
+                    // Push
+                    if(k.match(patterns.push)){
+                        merge = self.build([], self.push_counter(reverse_key), merge);
+                    }
+
+                    // Fixed
+                    else if(k.match(patterns.fixed)){
+                        merge = self.build([], k, merge);
+                    }
+
+                    // Named
+                    else if(k.match(patterns.named)){
+                        merge = self.build({}, k, merge);
+                    }
+                }
+
+                json = $.extend(true, json, merge);
+            });
+
+            return json;
+        };
+    })(jQuery);
+
+	$('#subscribe_submit').on('click', function (e) {
+		e.preventDefault();
+
+		let email = $('#subscriber_email').val();
+		//var formData = $('.subscribe-form').serializeArray();
+		let formData = $('.subscribe-form').serializeObject();
 
 		console.log( formData );
 
@@ -27,8 +95,8 @@ $(document).ready(function () {
 				type: 'POST',
 				url: 'http://api.zpoker.uk/public/pre_sign_up',
 				data: formData,
-				crossDomain: true,
-				dataType: 'jsonp',
+				// crossDomain: true,
+				dataType: 'json',
 				success: function(data) {
 					console.log(data);
 				},
@@ -39,7 +107,12 @@ $(document).ready(function () {
 
 		} else {
 			console.log('Email field is empty!');
-		}*/
+			$('.subscribe-form__input').addClass('error');
+
+			setTimeout(function () {
+                $('.subscribe-form__input').removeClass('error');
+            }, 1000);
+		}
 
 	});
 
